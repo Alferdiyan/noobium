34be5fe8f32bf483ed6d6cd54e3ca8873ff8a909
Alferdiyan <ferdi1505@gmail.com>
2022-12-02 10:11:39 +0700

    Add Sign In auth

---
+++ pages/auth/sign-in.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Button from '../../components/Button';
+
+const SignInPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Sign In | Noobium</title>
+        <h1>ini sign in</h1>
+      </Head>
+      <Button>Sign In</Button>
+    </div>
+  );
+};
+
+export default SignInPage;
